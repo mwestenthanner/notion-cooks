17e8864dfbb03484a2a22326c8eaa854d98244a8
Marianne Westenthanner <marianne@westenthanner.de>
2022-06-14 08:24:19 +0200

    Basic frontend setup, added Pinia

---
+++ app/src/components/RecipePanel.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="panel">This is a recipe</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'RecipePanel'
+});
+</script>
+
+<style scoped>
+
+</style>
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+createApp(App)
+    .mount('#app')
+    
+App.use(createPinia())
