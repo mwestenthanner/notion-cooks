81524b64be8018b080985a5386efa3f95d6a2064
Marianne Westenthanner <marianne@westenthanner.de>
2022-06-14 09:52:17 +0200

    Added CORS dependency

--- express/api.js
+++ express/api.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const cors = require('cors');
 const { getRecipes, getRecipeContent } = require('../services/notion');
 
 const app = express();
